0724761831428d56e30ee67d7f82b6fedc84a405
gabe <gabrielaliphas@gmail.com>
2026-06-11 11:47:58 -0400

    done

--- src/App.java
+++ src/App.java
@@ -1,15 +1,24 @@
 public class App {
-	public static void main(String[] args){
+	public static void main(String[] args) {
 		int[][] a = {
-			{1,2,3},
-			{4,5,6},
-			{1,3,5}
+
+				{ 1, 2, 3 },
+				{ 4, 5, 6 },
+				{ 1, 3, 5 }
 		};
-		System.out.println(sum2D(a)); //should print 30
+
+		System.out.println(sum2D(a)); // should print 30
 	}
-	
-	public static int sum2D(int[][] nums){
-		return -1;
+
+	public static int sum2D(int[][] nums) {
+		int sum = 0;
+
+		for (int i = 0; i < nums.length; i++) {
+			for (int j = 0; j < nums[i].length; j++) {
+				sum += nums[i][j];
+			}
+
+		}
+		return sum;
 	}
-	
 }
